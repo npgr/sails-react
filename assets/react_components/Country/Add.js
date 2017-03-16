624aba8ba8e13e9d54d8852e8170a13eb5d878bf
Nuno Goncalves <e_npgr@yahoo.com>
2017-03-16 17:37:18 -0400

    React Component "Add" (Country)

---
+++ assets/react_components/Country/Add.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import store from './CountryStore'
+
+export default class Add extends React.Component {
+	render() {
+		return(
+			<div>
+				Add Country V2
+			</div>
+		)
+	}
+}
